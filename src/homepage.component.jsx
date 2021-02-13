0348cc547842a3fa215052329f70647c00dffa33
Daphne Fang <daphne.fang@criteriacorp.com>
2021-02-12 16:26:20 -0800

    init repo

---
+++ src/homepage.component.jsx
@@ -0,0 +1,41 @@
+import React from 'react'
+import './homepage.styles.scss'
+
+export const Homepage = props => {
+    return (
+        <div className='homepage'>
+            <div className='directory-menu'>
+                <div className="menu-item">
+                    <div className='content'>
+                        <h2 className='title'>HATS</h2>
+                        <span className='subtitle'>SHOP NOW</span>
+                    </div>
+                </div>
+                <div className="menu-item">
+                    <div className='content'>
+                        <h2 className='title'>HATS</h2>
+                        <span className='subtitle'>SHOP NOW</span>
+                    </div>
+                </div>
+                <div className="menu-item">
+                    <div className='content'>
+                        <h2 className='title'>HATS</h2>
+                        <span className='subtitle'>SHOP NOW</span>
+                    </div>
+                </div>
+                <div className="menu-item">
+                    <div className='content'>
+                        <h2 className='title'>HATS</h2>
+                        <span className='subtitle'>SHOP NOW</span>
+                    </div>
+                </div>
+                <div className="menu-item">
+                    <div className='content'>
+                        <h2 className='title'>HATS</h2>
+                        <span className='subtitle'>SHOP NOW</span>
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
